a1623a0c204bb471f063332ca2a46c1cddd07b8a
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-07-21 23:40:15 +0100

    implemented the summary page

---
+++ summary.js
@@ -0,0 +1,7 @@
+// Success Popup
+const confirmBtn = document.querySelector("#confirm-btn")
+const successPop = document.querySelector(".success-pop")
+
+confirmBtn.addEventListener("click", ()=>{
+  successPop.style.display = "block"
+})
